ac05e4de12f552d6b58ca85a5e7c2737750b3ccd
pjehan <pierre.jehan@gmail.com>
2020-01-25 17:49:52 +0100

    Correction du nom des images dans les fixtures

--- src/DataFixtures/ProjectFixtures.php
+++ src/DataFixtures/ProjectFixtures.php
@@ -13,7 +13,7 @@ class ProjectFixtures extends Fixture implements DependentFixtureInterface
     {
         $project1 = new Project();
         $project1->setName("Good Girl");
-        $project1->setImage("project1.png");
+        $project1->setImage("project1.jpg");
         $project1->setExcerpt("Ce film parle de ...");
         $project1->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed auctor leo. In ullamcorper felis orci, at vestibulum velit blandit et. Curabitur enim nunc, fermentum quis magna ultricies, bibendum mollis elit. Donec at gravida velit. In et lectus pharetra, egestas elit non, vehicula purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus ultricies ipsum libero. Aliquam mollis, nulla sed lobortis rhoncus, urna felis elementum ex, in egestas turpis dolor et nibh.");
         $project1->setGoal(5500);
@@ -35,7 +35,7 @@ class ProjectFixtures extends Fixture implements DependentFixtureInterface
 
         $project3 = new Project();
         $project3->setName("Dabado");
-        $project3->setImage("project3.png");
+        $project3->setImage("project3.jpg");
         $project3->setExcerpt("Un jeu fantastique peint à la main. Plongez dans des aventures extra-ordinaires !");
         $project3->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed auctor leo. In ullamcorper felis orci, at vestibulum velit blandit et. Curabitur enim nunc, fermentum quis magna ultricies, bibendum mollis elit. Donec at gravida velit. In et lectus pharetra, egestas elit non, vehicula purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus ultricies ipsum libero. Aliquam mollis, nulla sed lobortis rhoncus, urna felis elementum ex, in egestas turpis dolor et nibh.");
         $project3->setGoal(4850);
@@ -46,7 +46,7 @@ class ProjectFixtures extends Fixture implements DependentFixtureInterface
 
         $project4 = new Project();
         $project4->setName("DOOSH");
-        $project4->setImage("project4.png");
+        $project4->setImage("project4.jpg");
         $project4->setExcerpt("Venez m'accompagner dans mon projet de création musicale avec clip vidéo !");
         $project4->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed auctor leo. In ullamcorper felis orci, at vestibulum velit blandit et. Curabitur enim nunc, fermentum quis magna ultricies, bibendum mollis elit. Donec at gravida velit. In et lectus pharetra, egestas elit non, vehicula purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus ultricies ipsum libero. Aliquam mollis, nulla sed lobortis rhoncus, urna felis elementum ex, in egestas turpis dolor et nibh.");
         $project4->setGoal(9800);
